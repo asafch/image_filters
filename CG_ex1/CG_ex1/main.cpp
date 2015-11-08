31b833afe707fb191caf9f33988ce90d63ad69db
asafch <asaf.chelouche@gmail.com>
2015-11-08 12:37:07 +0200

    Initial resources

---
+++ CG_ex1/CG_ex1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CG_ex1
+//
+//  Created by Asaf Chelouche on 8/11/15.
+//  Copyright © 2015 asafch\bbc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
